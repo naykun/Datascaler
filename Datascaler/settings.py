db5d5bfcc2a16eef5531289f81d17f7f641edbe0
闫坤 <yankun1138283845@foxmail.com>
2018-02-27 16:50:26 +0800

    build project structure

--- Datascaler/settings.py
+++ Datascaler/settings.py
@@ -77,8 +77,7 @@ WSGI_APPLICATION = 'Datascaler.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': None,
     }
 }
 
@@ -120,5 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = (os.path.join(BASE_DIR,"static"),)
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
+
+
+from mongoengine import connect
+connect('datascaler')
--- datascaler_web/models.py
+++ datascaler_web/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
+from mongoengine import *
 # Create your models here.
+
+class DataInfo(Document):
+    des = StringField()
