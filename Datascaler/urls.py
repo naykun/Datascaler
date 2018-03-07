a8547d2e6f246c617eafdd6c58b7c9dabb1227bd
闫坤 <yankun1138283845@foxmail.com>
2018-03-07 13:22:44 +0800

    setup web structure

--- Datascaler/urls.py
+++ Datascaler/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
-from datascaler_web.views import index
+from datascaler_web.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^index/',index),
+    url(r'^chart/$',chart),
+    url(r'^doc/$',doc)
 ]
--- datascaler_web/views.py
+++ datascaler_web/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,"index.html")
+
+def chart(request):
+    return render(request,"chart.html")
+def doc(request):
+    return render(request,"doc.html")
