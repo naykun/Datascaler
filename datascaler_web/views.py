4b8e9badcb345171b749780a4c22cefe1bbda165
sfzyk <767039957@qq.com>
2018-03-17 22:04:12 +0800

    add a interface for airquality

--- datascaler_web/views.py
+++ datascaler_web/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from models import *
+import json
 # Create your views here.
 def index(request):
     return render(request,"index.html")
@@ -8,3 +9,12 @@ def chart(request):
     return render(request,"chart.html")
 def doc(request):
     return render(request,"doc.html")
+def air(request):
+    air_date=request.GET.get("time")
+    sta_id=request.GET.get("station")
+    try :
+        item=airquality.objects(time=air_date,station_id=sta_id)[0]
+        info=json.loads(item.to_json())
+    except:
+        info={"error":"nosuchinfo"}
+    return render(request,"xxx.html",info)
