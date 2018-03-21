243bff8f5a8ba59835059f2cfb07745bfe23f92e
sfzyk <767039957@qq.com>
2018-03-21 14:42:52 +0800

    return json

--- datascaler_web/views.py
+++ datascaler_web/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from datascaler_web.models import *
+from django.http import JsonResponse
 import json
 import re
 # Create your views here.
 def index(request):
     return render(request,"index.html")
-
 def chart(request):
     return render(request,"chart.html")
 def doc(request):
@@ -36,24 +36,21 @@ def process(month):
     if month=='Dec':
         return '12'
 def air(request):
-    print(request.GET)
-    #pat=re.compile(r'\d{4}.*\d{2}\:\d{2}\:\d{2}')
     air_date=request.GET.get("time").split(' ')
     air_date=air_date[3]+' '+process(air_date[1])+" "+air_date[2]+" "+air_date[4]
-    #air_date=pat.findall(air_date)[0]
-    sta_id=int(request.GET.get("station_id"))
-    print(air_date)
-    print(sta_id)
     try :
-        item=airquality.objects(time=air_date,station_id=sta_id)[0]
-        info=json.loads(item.to_json())
-        sta=station.objects(station_id=sta_id)[0]
-        info2 = json.loads(sta.to_json())
-        #print(info2)
-        #print(info)
-        info['latitude']=info2['latitude']
-        info['longitude']=info2['longitude']
+        items=airquality.objects(time=air_date)
+        return_items={}
+        for i,item in enumerate(items):
+            info=json.loads(item.to_json())
+            if (item['station_id']>=1001 and item['station_id']<=1036) or(item['station_id']>=6001 and item['station_id']<=6028):
+                sta=station.objects(station_id=info['station_id'])[0]
+                info2 = json.loads(sta.to_json())
+                info['latitude']=info2['latitude']
+                info['longitude']=info2['longitude']
+                info['name_chinese']=info2['name_chinese']
+                return_items[i]=info
     except:
         info={"error":"nosuchinfo"}
         print("an error //oops\n\n\n\n\n\n\n\n\n\n\n ")
-    return render(request,"chart.html",info)
+    return JsonResponse(info)
